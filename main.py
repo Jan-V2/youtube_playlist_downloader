d0afa105557573c14b9b0fdeea5e62e360a40be6
jan <bla@bla.com>
2020-03-08 16:22:47 +0100

    updated to python 3.8 and pytube3

--- main.py
+++ main.py
@@ -10,8 +10,8 @@ if __name__ == '__main__':
     init_db()
     download_playlist(1)
     pl_urls = ["https://www.youtube.com/playlist?list=PLJ_TJFLc25JQaIKha3sduCHUXkFcNMMDS", "https://www.youtube.com/playlist?list=UUu6mSoMNzHQiBIOCkHUa2Aw"]
-    insert_playlist(pl_urls[0], "/home/john/test", "RLM trailers")
-    insert_playlist(pl_urls[1], "/home/john/test2", "cody's lab channel playlist")
+    #insert_playlist(pl_urls[0], "/home/john/test", "RLM trailers")
+    #insert_playlist(pl_urls[1], "/home/john/test2", "cody's lab channel playlist")
 
     for pl_url in pl_urls:
          pl = Playlist(pl_url)
